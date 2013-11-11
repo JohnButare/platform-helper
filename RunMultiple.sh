722f1354f83b65fc2f90bf8d8555c81b3d23ee2c
jjbutare <john.butare.s@gmail.com>
2013-11-10 17:08:30 -0700

    script changes

--- RunMultiple.sh
+++ RunMultiple.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . function.sh
-eval "$@" # allow multiple commands, i.e. "sudo RunMultiple.sh 'ls; pause'"
+eval "$@" # allow multiple commands, i.e. "sudo RunMultiple.sh 'ls; pause'", where can't use bash -c
 exit $?
