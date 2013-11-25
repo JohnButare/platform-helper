9edcedfe90db205ec31abb1120ffb9fe6a558c5f
jjbutare <john.butare.s@gmail.com>
2013-11-24 17:58:01 -0700

    script changes

--- test.sh
+++ test.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-. function.sh
-echo $PUBLIC
+
+a()
+{
+	result=$(date "+%Y_%m_%d %H_%M_%S" -d @1385329140.000000000)
+}
+
+b()
+{
+	date "+%Y_%m_%d %H_%M_%S" -d @1385329140.000000000
+}
+
+time a
+time result="$(b)"
