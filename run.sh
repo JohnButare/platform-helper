5d6128975b7b5f8c14da4ea1cc2c975f11d57be2
jjbutare <john.butare.s@gmail.com>
2013-12-25 12:35:09 -0700

    mac changes

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 . function.sh
 # allow multiple commands, i.e. "sudo RunMultiple.sh 'ls; pause'", where can't use bash -c
 declare multiple; [[ $1 == @(--multiple|-m) ]] && { multiple="true"; shift; }
