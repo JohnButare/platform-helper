d3e1e0a0dad1d868c322c72c257e93bf5d5a08db
John Butare <john.butare@intel.com>
2015-05-04 09:22:11 -0600

    word: add support for Mac templates

--- office.sh
+++ office.sh
@@ -1,11 +1,15 @@
 OfficeVersion="15"
 OfficeTitle=".* - "
+WordProgram=""
 WordFastStartTitle="Word Fast Start.* - Word"
 OfficeArchitecture="x86"
 OfficeBits="32"
 Office365=""
 
-if [[ -f "$P/Microsoft Office 15/root/office15/WinWord.exe" ]]; then
+if [[ -d "$P/Microsoft Word.app" ]]; then
+	WordProgram="$P/Microsoft Word.app"
+
+elif [[ -f "$P/Microsoft Office 15/root/office15/WinWord.exe" ]]; then
 	OfficeDir="$P/Microsoft Office 15/root/office15"
 	Office365="true"
 
@@ -32,22 +36,12 @@ elif [[ -f "$P32/Microsoft Office/Office14/WinWord.exe" ]]; then
 	OfficeTitle="* - Microsoft"
 	WordFastStartTitle="Word Fast Start.* - Microsoft Word"
 	
-elif [[ -f "$P32/Microsoft Office/Office12/WinWord.exe" ]]; then
-	OfficeDir="$P32/Microsoft Office/Office12"
-	OfficeVersion="11"
-	OfficeTitle="* - Microsoft"
-	WordFastStartTitle="Word Fast Start.* - Microsoft Word"
-	
-elif [[ -f "$P32/Microsoft Office/Office11/WinWord.exe" ]]; then
-	OfficeDir="$P32/Microsoft Office/Office11"
-	OfficeVersion="11"
-	OfficeTitle="* - Microsoft"
-	WordFastStartTitle="Word Fast Start.* - Microsoft Word"
-
 else
 	OfficeDir=""
 	return 1
 fi
+
+[[ ! $WordProgram ]] && WordProgram="$OfficeDir/WinWord.exe"
 
 # x64 Office if we are running under x64 OS and Office is installs to $P
 if [[ "$(OsArchitecture)" == "x64" && "$OfficeDir" =~ ^$P/ ]]; then
