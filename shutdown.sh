4caf8f40288c7cef9ad93657594c5d8194533202
John Butare <john.butare@intel.com>
2019-04-08 19:01:15 +0100

    vmware: add

--- shutdown.sh
+++ shutdown.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 . function.sh
 
-function log() { echo "$(GetTimeStamp): $1" >> /var/log/shutdown.log; }
+log="/var/log/shutdown.log"
+function log() { echo "$(GetTimeStamp): $1" | tee -a "$log"; }
 
 log "system is shutting down"
-
+vmware close | tee -a "$log"
 log "shutdown script has finished"
