b23536c26989f3f768cc8d5adb56016302d7328b
John Butare <john@butare.net>
2020-07-06 13:56:57 -0600

    HostUpdate: add colors to logging and Oh My Zsh plugin and theme support

---
+++ color.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Use colors, but only if connected to a terminal, and that terminal
+# supports them.
+if [ -t 1 ]; then
+  RB_RED=$(printf '\033[38;5;196m')
+  RB_ORANGE=$(printf '\033[38;5;202m')
+  RB_YELLOW=$(printf '\033[38;5;226m')
+  RB_GREEN=$(printf '\033[38;5;082m')
+  RB_BLUE=$(printf '\033[38;5;021m')
+  RB_INDIGO=$(printf '\033[38;5;093m')
+  RB_VIOLET=$(printf '\033[38;5;163m')
+
+  RED=$(printf '\033[31m')
+  GREEN=$(printf '\033[32m')
+  YELLOW=$(printf '\033[33m')
+  BLUE=$(printf '\033[34m')
+  BOLD=$(printf '\033[1m')
+  RESET=$(printf '\033[m')
+else
+  RB_RED=""
+  RB_ORANGE=""
+  RB_YELLOW=""
+  RB_GREEN=""
+  RB_BLUE=""
+  RB_INDIGO=""
+  RB_VIOLET=""
+
+  RED=""
+  GREEN=""
+  YELLOW=""
+  BLUE=""
+  BOLD=""
+  RESET=""
+fi
