24facf82bcda99d38312820065a6d76a3153e1af
John Butare <john@butare.net>
2020-08-13 18:35:43 -0600

    wiggin: updates for Kea and bind9

--- bootstrap-config.sh
+++ bootstrap-config.sh
@@ -1,9 +1,13 @@
 user="jjbutare" 									# user to run bootstrap as
+timezone="America/Denver"					# host timezone
+
 domain="hagerman.butare.net" 
+dns1="192.168.100.10"
+dns2="192.168.100.11"
+workgroup="WIGGIN"
+
 host="nas3.$domain" 							# host or mounted drive to use for scripts and installers
 proxy="http://proxy.$domain:3128" # proxy server for package download
-timezone="America/Denver"
-workgroup="WIGGIN"
 
 #user=""
 #host="/mnt/d"
--- test.sh
+++ test.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env zsh
 . function.sh || exit
 
-	case "aa$HOSTNAME" in
-		jane|pants) i $mac || return;;
-		oversoul) i hp9020 $pc || return;;
-		rosie) i MsiMotherboard $pc || return;;
-		ultron) i $pc || return;;
-	esac
+ cat kea-dhcp4-wiggin-reservations.json | sed '/^[	 ]*\/\//d' | head -10
