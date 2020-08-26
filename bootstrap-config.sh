9b06f3fb41e43f363d128a1dd81e6ee1ceb2b4e5
John Butare <john@butare.net>
2020-08-26 14:31:22 -0600

    add SMB Workgroup support for Linux

--- bootstrap-config.sh
+++ bootstrap-config.sh
@@ -5,7 +5,7 @@ locale="en_US.UTF-8"							# host locale
 domain="hagerman.butare.net" 
 dns1="192.168.100.10"
 dns2="192.168.100.11"
-workgroup="WIGGIN"
+workgroup="Wiggin"
 
 host="nas3.$domain" 							# host or mounted drive to use for scripts and installers
 proxy="http://proxy.$domain:3128" # proxy server for package download
