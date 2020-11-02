27c8a4926ee56234ecd0a04300f1e6044fe3a067
John Butare <john@butare.net>
2020-11-02 15:08:07 -0700

    hashi: add consul encryption

--- bootstrap-config.sh
+++ bootstrap-config.sh
@@ -2,12 +2,13 @@ user="jjbutare" 									# user to run bootstrap as
 timezone="America/Denver"					# host timezone
 locale="en_US.UTF-8"							# host locale
 
-domain="hagerman.butare.net" 
-dns1="192.168.100.10"
-dns2="192.168.100.11"
+baseDomain="butare.net"
 workgroup="hagerman"
+domain="$workgroup.$baseDomain"
 systemUser="wsystem"
 
+dns1="192.168.100.10"
+dns2="192.168.100.11"
 host="nas3.$domain" port=608			# host or mounted drive to use for scripts and installers
 proxy="http://proxy.$domain:3128" # proxy server for package download
 
