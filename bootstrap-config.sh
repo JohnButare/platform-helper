5e7b76f6fa675367348a59d1cdb48ec25e7c8d8d
John Butare <johns@butare.net>
2021-02-13 18:22:36 -0700

    wiggin update: refactor

--- bootstrap-config.sh
+++ bootstrap-config.sh
@@ -2,13 +2,17 @@ user="jjbutare"						# run the bootstrap as this user
 timezone="America/Denver"
 locale="en_US.UTF-8"
 
+network="wiggin"
 workgroup="hagerman"
 baseDomain="butare.net"
 domain="$workgroup.$baseDomain"
 systemUser="wsystem"
 
+dnsServers="pi1,pi2"
 dns1="192.168.100.10"
 dns2="192.168.100.11"
+
+dhcpServers="pi1,pi2"
 
 proxyNetworks="[wiggin]=192.168.100.10" 
 proxyServer="proxy.$domain" proxyPort="3128"
