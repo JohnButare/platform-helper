d1b3c00a36326a267c2560cb3c6eace61fc9cf34
John Butare <johns@butare.net>
2021-05-09 22:04:23 -0600

    change Wiggin LAN network to 10.10.100.0/22

--- bootstrap-config.sh
+++ bootstrap-config.sh
@@ -10,15 +10,15 @@ domain="$workgroup.$baseDomain"
 systemUser="wsystem"
 
 dnsServers="pi2,pi1" # update pi2 first
-dns1="192.168.100.10"
-dns2="192.168.100.11"
+dns1="10.10.100.10"
+dns2="10.10.100.11"
 
 dhcpServers="pi2,pi1" # update pi2 first
 
 hostTimeout="200" # host discovery timeout in milliseconds
 hostWaitTimeout="200" # seconds to wait for host to be available
 
-proxyNetworks="[wiggin]=192.168.100.10" 
+proxyNetworks="[wiggin]=10.10.100.10" 
 proxyServer="proxy.$domain" proxyPort="3128"
 proxy="http://$proxyServer:$proxyPort"
 
