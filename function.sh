90c19f86f679e060b166c7f4ecad5705227fcd86
John Butare <johns@butare.net>
2021-10-30 18:08:04 -0600

    fix credential issues due to pipefail option
    - function.sh: add tgrep, grep which always returns true
    - credential: use tgrep in pipelines which should not fail if no records are returned

--- function.sh
+++ function.sh
@@ -2043,6 +2043,7 @@ sudoe()
 #
 
 tac() { InPath tac && command tac "$@" | cat "$@"; }
+tgrep() { grep "$@"; true; } # true grep, always returns true, useful in pipelines if pipefail is set and grep returns to lines
 Utf16toAnsi() { iconv -f utf-16 -t ISO-8859-1; }
 Utf16to8() { iconv -f utf-16 -t UTF-8; }
 
