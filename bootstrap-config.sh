4aa7765e58d5c196956a38f4a35c588c522f54ed
John Butare <johns@butare.net>
2022-06-27 14:10:57 -0600

    add additional DNS and DHCP servers

--- bootstrap-config.sh
+++ bootstrap-config.sh
@@ -18,38 +18,28 @@ domain="$network.$baseDomain"
 systemUser="wsystem"
 confDir="$CLOUD/network/system"
 
+#
+# servers
+#
+
+# DHCP
+dhcpServers="pi1,pi2,pi3,pi4"
+
 # DNS
-dnsServers="pi2,pi1" # update pi2 first
-dns1="10.10.100.10"
-dns2="10.10.100.11"
+dnsServers="pi1,pi2,pi3,pi4"
+dns1="10.10.100.7"
+dns2="10.10.100.6"
 
 # proxy
 noProxy="localhost,127.0.0.1,.$baseDomain,.$domain,web,www,autoproxy,.releases.ubuntu.com"
 
-# time
-timezone="America/Denver"
-timeServer="time.butare.net"
-
-dhcpServers="pi2,pi1" # update pi2 first
+# other
 mqttServer="mosquitto"
 
-hostTimeout="200" # host discovery timeout in milliseconds
-hostWaitTimeout="200" # seconds to wait for host to be available
-
-# networks - list of available networks that the system can be connected to
-networks="hagerman@10.10.100.10,hagerman@10.10.100.11" # list of DNS servers for the specified network in the format NETWORK@IP
-hagermanBaseDomain="butare.net"
-hagermanDomain="hagerman.$hagermanBaseDomain"
-hagermanBackupUser="$user"
-hagermanBackupServers="backup3.$hagermanBaseDomain,backup2.$hagermanBaseDomain,backup1.$hagermanBaseDomain" # list of backup servers in the format HOST1 [,HOST2]...
-hagermanDockerServers="pi1.$hagermanBaseDomain,pi2.$hagermanBaseDomain" # Docker Swarm managers
-hagermanFileServers="file2.$hagermanBaseDomain,file1.$hagermanBaseDomain"
-hagermanNas="nas3.$hagermanDomain"
-hagermanProxyServers="proxy.$hagermanBaseDomain:3128"
-hagermanVip="10.10.100.3"
-hagermanWireguardServers="pi2.$hagermanBaseDomain,pi1.$hagermanBaseDomain"
-
+#
 # HashiCorp
+#
+
 hashiDnsDomain="service"
 hashiCredentialPath=""
 hashiCertificateDevice="$CDATA/VeraCrypt/personal.hc" hashiCertificateDir="data/hashi"
@@ -64,3 +54,20 @@ hashiTestCertificateDevice="$CDATA/VeraCrypt/personal.hc" hashiTestCertificateDi
 hashiTestServers="pi5,pi6,pi7"
 hashiTestClients="pi8"
 hashiTestVaultServers="pi5,pi6"
+
+#
+# networks
+#
+
+# networks - list of available networks that the system can be connected to
+networks="hagerman@10.10.100.10,hagerman@10.10.100.11" # list of DNS servers for the specified network in the format NETWORK@IP
+hagermanBaseDomain="butare.net"
+hagermanDomain="hagerman.$hagermanBaseDomain"
+hagermanBackupUser="$user"
+hagermanBackupServers="backup3.$hagermanBaseDomain,backup2.$hagermanBaseDomain,backup1.$hagermanBaseDomain" # list of backup servers in the format HOST1 [,HOST2]...
+hagermanDockerServers="pi1.$hagermanBaseDomain,pi2.$hagermanBaseDomain" # Docker Swarm managers
+hagermanFileServers="file2.$hagermanBaseDomain,file1.$hagermanBaseDomain"
+hagermanNas="nas3.$hagermanDomain"
+hagermanProxyServers="proxy.$hagermanBaseDomain:3128"
+hagermanVip="10.10.100.5"
+hagermanWireguardServers="pi2.$hagermanBaseDomain,pi1.$hagermanBaseDomain"
