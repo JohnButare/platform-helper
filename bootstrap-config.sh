36998557920c8c6fdcc890485101f2ff2c06e9f1
John Butare <johns@butare.net>
2022-07-30 10:29:59 -0600

    bootstrap-config.sh: update Hagerman servers

--- bootstrap-config.sh
+++ bootstrap-config.sh
@@ -48,9 +48,9 @@ hashiVaultServers="pi2,pi1"
 hashiTestCredentialPath=""
 #hashiTestCertificateDevice="" hashiTestCertificateDir="$UDATA/app/hashi"
 hashiTestCertificateDevice="$CDATA/VeraCrypt/personal.hc" hashiTestCertificateDir="data/hashi"
-hashiTestServers="pi5,pi6,pi7"
-hashiTestClients="pi8"
-hashiTestVaultServers="pi5,pi6"
+hashiTestServers="pi20,pi21"
+hashiTestClients="pi22"
+hashiTestVaultServers="pi20,pi21"
 
 #
 # networks
@@ -60,14 +60,17 @@ hashiTestVaultServers="pi5,pi6"
 networks="hagerman@10.10.100.10,hagerman@10.10.100.11" # list of DNS servers for the specified network in the format NETWORK@IP
 hagermanBaseDomain="butare.net" hbd="$hagermanBaseDomain"
 hagermanDomain="hagerman.$hagermanBaseDomain" hd="$hagermanDomain"
+hagermanCredentialPaths="CloudFlare,domotz,JumpCloud,LastPass,namecheap,ssh,system,unifi"
+hagermanNas="nas3.$hd"																			# NAS Server
+hagermanVip="10.10.100.6" 																	# Virtual IP Address
+
+hagermanCameraServers="BackShedCamera,BackYardEastCamera,ChickenYardNorthCamera,ChickenYardSouthCamera,FrontPatioCamera,FrontYardEastCamera,FrontYardWestCamera,LivingRoomCamera"
 hagermanDhcpServers="pi1,pi2,pi3,pi4"
 hagermanDnsServers="pi1,pi2,pi3,pi4"
 hagermanBackupUser="$user"
 hagermanBackupServers="backup2.$hbd,backup1.$hbd" 					# backup (Borg)
 hagermanHassServers="pi9.$hbd,pi11.$hbd"										# Home Assistant
 hagermanLbServers="pi1.$hd,pi2.$hd,pi3.$hd,pi4.$hd"					# Load Balancer (NGINX reverse proxy)
-hagermanNas="nas3.$hd"																			# NAS Server
 hagermanProxyServers="proxy.$hbd:3128" 											# Forward Proxy (Squid)
-hagermanVip="10.10.100.6" 																	# Virtual IP Address
 hagermanWireguardServers="pi4.$hd,pi3.$hd,pi2.$hd,pi1.$hd" 	# Wireguard VPN
 hagermanWireguardPort="51820"
