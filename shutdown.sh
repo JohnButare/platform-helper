97532de6d5d596bbc3125eea8d4bbd6d0dc3e56b
John Butare <johns@butare.net>
2023-03-24 13:13:19 -0600

    remove unused files

--- shutdown.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-. function.sh
-
-log="/var/log/shutdown.log"
-function log() { echo "$(GetTimeStamp): $1" | tee -a "$log"; }
-
-log "system is shutting down"
-vmware close | tee -a "$log"
-log "shutdown script has finished"
