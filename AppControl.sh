32d924bcc04e4e4686fc9ad824fe9ae74268614d
John Butare <john@butare.net>
2023-05-08 16:04:57 -0600

    AppCommand: fix AppIsInstalled and AppIsRunning

--- AppControl.sh
+++ AppControl.sh
@@ -4,9 +4,9 @@ AppClose() { AppCommand close "$1"; }
 AppCommandExists() { AppFunctionExists "${1}Command" "$2" ; } # AppCommandExists COMMAND APP - application supports command
 AppExists() { local app="$(AppHelper "$1")"; FindInPath "$app" > /dev/null && AppCommandExists isInstalled "$(FindInPath "$app")"; } # AppExists APP - return true if the application is a helper file
 AppFunctionExists() { local app="$(AppHelper "$2")"; [[ -f "$app" ]] && ${G}grep --quiet "^$1"'()' "$app"; } # AppFunctionExists FUNCTION APP - return true if the function exists in the app
-AppIsInstalled() { AppCommand IsInstalled "$1"; }
+AppIsInstalled() { AppCommand isInstalled "$1"; }
 AppInstallCheck() { AppIsInstalled "$1" && return; [[ ! $quiet ]] && ScriptErr "application is not installed" "$1"; return 1; }
-AppIsRunning() { AppCommand IsRunning "$1"; }
+AppIsRunning() { AppCommand isRunning "$1"; }
 AppStart() { AppCommand start "$1"; }
 AppStartRestore() { [[ ! $wasRunning ]] && return 0; AppStart "$1"; }
 
@@ -22,7 +22,9 @@ AppCloseSave()
 AppCommand()
 {
 	local command="$1" app="$2" appPath
+
 	appPath="$(FindInPath "$app")" || return
+
 	if AppCommandExists "$command" "$appPath"; then
 		[[ "$command" == @(start|startup) ]] && echo "Starting $app..."
 		[[ "$command" == @(close) ]] && echo "Closing $app..."
