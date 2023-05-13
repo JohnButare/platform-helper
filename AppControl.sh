d2f5a887b41eaa9137a0878a3f29443ae9f7eb0d
John Butare <john@butare.net>
2023-05-13 15:06:28 -0600

    inst: new features
    - add check command - check installed and download application versions
    - add install command- show installed applications

--- AppControl.sh
+++ AppControl.sh
@@ -4,8 +4,9 @@ AppClose() { AppCommand close "$1"; }
 AppCommandExists() { AppFunctionExists "${1}Command" "$2" ; } # AppCommandExists COMMAND APP - application supports command
 AppExists() { local app="$(AppHelper "$1")"; FindInPath "$app" > /dev/null && AppCommandExists isInstalled "$(FindInPath "$app")"; } # AppExists APP - return true if the application is a helper file
 AppFunctionExists() { local app="$(AppHelper "$2")"; [[ -f "$app" ]] && ${G}grep --quiet "^$1"'()' "$app"; } # AppFunctionExists FUNCTION APP - return true if the function exists in the app
-AppIsInstalled() { AppCommand isInstalled "$1"; }
 AppInstallCheck() { AppIsInstalled "$1" && return; [[ ! $quiet ]] && ScriptErr "application is not installed" "$1"; return 1; }
+AppIsHelper() { [[ "$(GetFilePath "$(FindInPath "$1")")" == "$BIN" ]]; }
+AppIsInstalled() { AppCommand isInstalled "$1"; }
 AppIsRunning() { AppCommand isRunning "$1"; }
 AppStart() { AppCommand start "$1"; }
 AppStartRestore() { [[ ! $wasRunning ]] && return 0; AppStart "$1"; }
@@ -18,19 +19,17 @@ AppCloseSave()
 	AppClose "$1"
 }
 
-# AppCommand COMMAND APP - execute COMMAND on APP if the command exists
+# AppCommand COMMAND APP - execute COMMAND on APP
 AppCommand()
 {
-	local command="$1" app="$2" appPath
+	local command="$1" app="$(AppHelper "$2")" appPath
 
 	appPath="$(FindInPath "$app")" || return
 
-	if AppCommandExists "$command" "$appPath"; then
-		[[ "$command" == @(start|startup) ]] && echo "Starting $app..."
-		[[ "$command" == @(close) ]] && echo "Closing $app..."
-		"$app" "$command" || return
-	fi
-	return 0
+	! AppCommandExists "$command" "$appPath" && return 1
+	[[ "$command" == @(start|startup) ]] && echo "Starting $app..."
+	[[ "$command" == @(close) ]] && echo "Closing $app..."
+	"$app" "$command"
 }
 
 # AppHelper APP - return the helper application for the app
@@ -40,4 +39,3 @@ AppHelper()
 	[[ "$app" == @(1Password) ]] && app="${app}Helper"
 	echo "$app"
 }
-
