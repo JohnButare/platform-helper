20e3622f8d9a42cf59de1c2eb396f40a90b762d9
John Butare <johns@butare.net>
2023-07-20 10:07:55 -0600

    add rp1 and rp2 as servers

--- bootstrap-config.sh
+++ bootstrap-config.sh
@@ -46,8 +46,8 @@ mqttServer="mosquitto"
 hashiDnsDomain="service"
 hashiCredentialPath=""
 hashiCertificateDevice="$CLOUD/data/app/CryFS/personal" hashiCertificateDir="data/hashi"
-hashiServers="pi1,pi2,pi3"
-hashiClients="pi4"
+hashiServers="pi1,pi2,pi3,pi4"
+hashiClients="pi6,pi7,pi9,pi10,pi11,rp1,rp2"
 hashiVaultServers="pi2,pi1"
 
 # HashiCorp Testing
