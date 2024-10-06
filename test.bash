f836113a31c4bd65433d98f70b91a3bcc679eb42
John Butare <john@butare.net>
2024-10-06 17:15:53 -0600

    use PLATFORM_DIR to load scripts

--- test.bash
+++ test.bash
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-. app.sh || exit
+PLATFORM_DIR="${BASH_SOURCE[0]%/*}"
+. "$PLATFORM_DIR/function.sh" || exit
+. "$PLATFORM_DIR/app.sh" || exit
+. "$PLATFORM_DIR/color.sh" || exit
 
 echo "test BASH script"
--- test.zsh
+++ test.zsh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
-. function.sh || exit
-. $ZPLUG_REPOS/mafredri/zsh-async/async.zsh
+PLATFORM_DIR="${${(%):-%x}:h}"
+PLATFORM_DIR="/usr/local/data/bin"
+. "$PLATFORM_DIR/function.sh" || exit
+. "$PLATFORM_DIR/app.sh" || exit
+. "$PLATFORM_DIR/color.sh" || exit
+SourceIfExists "$ZPLUG_REPOS/mafredri/zsh-async/async.zsh" || exit
+
 
 echo "test ZSH script"
+ScriptDir
