d968bb6f71a14a425d43df69da9bc1fc9c79b330
John Butare <johns@butare.net>
2024-11-26 17:01:58 -0700

    function.sh: add PortScan

--- function.sh
+++ function.sh
@@ -1851,6 +1851,7 @@ IsHostnameVm() { [[ "$(GetWord "$1" 1 "-")" == "$(os name)" ]]; } 										# Is
 IsIpInCidr() { ! InPath nmap && return 1; nmap -sL -n "$2" | grep --quiet " $1$"; }		# IsIpInCidr IP CIDR - true if IP belongs to the CIDR, i.e. IsIpInCidr 10.10.100.10 10.10.100.0/22
 NetworkCurrent() { UpdateGetForce "network"; }; 																# NetworkCurrent - configured current network
 NetworkOld() { UpdateGetForce "network-old"; }; 																# NetworkOld - the previous network
+PortScan() { local args=(); IsPlatform win && [[ ! $1 ]] && args+=(-Pn); nmap "${1:-localhost}" "${args[@]}" "$@"; }
 RemovePort() { GetArgs; echo "$1" | cut -d: -f 1; }															# RemovePort NAME:PORT - returns NAME
 SmbPasswordIsSet() { sudoc pdbedit -L -u "$1" >& /dev/null; }										# SmbPasswordIsSet USER - return true if the SMB password for user is set
 UrlExists() { curl --output /dev/null --silent --head --fail "$1"; }						# UrlExists URL - true if the specified URL exists
