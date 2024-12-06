b28d95b45e1d4b1ccad8db25e7ada106f84c7266
John Butare <johns@butare.net>
2024-12-05 20:21:43 -0700

    bootstrap fixes

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -7,32 +7,31 @@ REM - 2: run the bootstrap process for WSL 2 (requires Hyper-V nested virtualiza
 REM variables
 set dir=%~dp0
 set wsl=%1
-if not DEFINED wsl set wsl=2
 set dist=Ubuntu
-set distUser=jjbutare
-rem set distUser=%USERNAME%
-
-REM distribution image - optional
+set distUser=%USERNAME%
 rem set distUnc=\\ender.hagerman.butare.net\public
 rem set distImage=documents\data\install\platform\linux\wsl\image\ubuntu\default.tar.gz
-
-REM create and run bootstrap.cmd on the users Desktop
-
-REM distribution
 
 REM unc - the current directory is a UNC, but we need to remove the trailing slash or net use will error
 set unc=%dir:~0,-1%  
 
+if not DEFINED wsl set wsl=2
+
+REM
 REM create bootstrap.cmd on the Desktop
+REM 
+
 set f=%HOMEDRIVE%%HOMEPATH%\Desktop\bootstrap.cmd
-> "%f%" echo @echo off
->> "%f%" echo echo ************************* bootstrap.cmd *************************
->> "%f%" echo set dist=%dist%
->> "%f%" echo set distUser=%distUser%
->> "%f%" echo set distUnc=%distUnc%
->> "%f%" echo set distImage=%distImage%
->> "%f%" echo if not exist %unc% net use %unc% /user:%distUser%
->> "%f%" echo %dir%bootstrap-cmd%wsl%.cmd
+if not exist "%f%" (
+	> "%f%" echo @echo off
+	>> "%f%" echo echo ************************* bootstrap.cmd *************************
+	>> "%f%" echo set dist=%dist%
+	>> "%f%" echo set distUser=%distUser%
+	>> "%f%" echo set distUnc=%distUnc%
+	>> "%f%" echo set distImage=%distImage%
+	>> "%f%" echo if not exist %unc% net use %unc% /user:%distUser%
+	>> "%f%" echo %dir%bootstrap.cmd
+)
 
 REM
 REM install WSL
