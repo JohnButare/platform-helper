328708b57bd729c52ed7eda56a001eacc06d133b
John Butare <john@butare.net>
2025-09-01 16:38:43 -0600

    function.sh ConfigExists and ConfigGet: align help

--- function.sh
+++ function.sh
@@ -968,8 +968,8 @@ ZoxideConf()
 # Config
 #
 
-ConfigExists() { local file; configInit "$2" && (. "$file"; IsVar "$1"); }							# ConfigExists VAR [FILE] - return true if a configuration variable exists
-ConfigGet() { local file; configInit "$2" && (. "$file"; eval echo "\$$1"); }						# ConfigGet VAR [FILE] - get a configuration variable
+ConfigExists() { local file; configInit "$2" && (. "$file"; IsVar "$1"); }				# ConfigExists VAR [FILE] - return true if a configuration variable exists
+ConfigGet() { local file; configInit "$2" && (. "$file"; eval echo "\$$1"); }			# ConfigGet VAR [FILE] - get a configuration variable
 ConfigGetCurrent() { ConfigGet "$(NetworkCurrent)$(UpperCaseFirst "$1")" "$2"; } 	# ConfigGetCurrent VAR [FILE] - get a configuration entry for the current network
 
 # configInit [FILE] - set the configuration file, find in $BIN/bootstrap-config.sh or /usr/local/bin/bootstrap-config.sh
