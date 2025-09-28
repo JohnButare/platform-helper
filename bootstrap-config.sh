afe7a6f8b0908e847270aed61ee0d970464f44ea
John Butare <johns@butare.net>
2025-09-28 10:23:06 -0600

    boostrap-config.sh hashiVaultServers: update

--- bootstrap-config.sh
+++ bootstrap-config.sh
@@ -27,7 +27,7 @@ hashiCredentialPath=""
 hashiCertificateDevice="$CLOUD/data/app/CryFS/personal" hashiCertificateDir="data/hashi"
 hashiServers="pi1,pi2,pi3,pi4,pi5"
 hashiClients="pi6,pi7,pi8,pi9,pi10,pi11,rp1,rp2,rp3,rp4"
-hashiVaultServers="pi5,pi4,pi3,pi2,pi1"
+hashiVaultServers="pi4,pi3,pi1"
 
 # HashiCorp Testing
 hashiTestCredentialPath=""
