6727b2d15798c6fb1830903fde36770b5147b2f0
John Butare <johns@butare.net>
2025-12-24 16:22:46 -0700

    inst IntelWirelessDriver: add version

--- function.sh
+++ function.sh
@@ -312,7 +312,7 @@ AppVersion()
 	fi
 
 	# Windows winget package
-	if [[ ! $version && "$app" == @(AMD Chipset Software|AMD Radeon Software|Realtek Ethernet Controller) ]] && IsPlatform win; then
+	if [[ ! $version && "$app" == @(AMD Chipset Software|AMD Radeon Software|Intel(R) Wireless Bluetooth|Realtek Ethernet Controller) ]] && IsPlatform win; then
 		version="$(PackageVersionWin "$app")"
 		(( $? != 0 )) && { appNotInstalled "$appOrig"; return; }
 	fi
